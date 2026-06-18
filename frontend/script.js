14c171103e3563a042170efee24ddb992097ce65
Rasmus Stenlund <rasmus.stenlund@gmail.com>
2026-06-18 19:03:25 +0200

    Add finished input for empirical

--- frontend/script.js
+++ frontend/script.js
@@ -139,37 +139,46 @@ function get_empirical_page() {
     return `
         <div class = "empirical">
             <div class = "enter">
+                <p>Composition</p>
                 <div class = "composition-maker">
-                    <div class = "empirical-part">
-                        <input type = "text" class = "empirical-element">
-                        <span>:</span>
-                        <input type = "text" class = "empirical-mass_percentage">
+                    <div class = "empirical-part-list">
+                        <div class = "empirical-part">
+                            <input type = "text" class = "empirical-element" placeholder = "Element">
+                            <span>:</span>
+                            <input type = "text" class = "empirical-mass_percentage" placeholder = "Mass %">
+                        </div>
                     </div>
+
                     <input type = "button" class = "add-button" id = "empirical-add-element" value = "+ Element">
                 </div>
 
                 <div class = "empirical-optional">
                     <div class = "optional-molar-mass">
-                        <span>(Optional) Molar mass</span>
+                        <p>(Optional) Molar mass</p>
                         <input type = "text" id = "molar-mass">
                     </div>
                     <div class = "optional-hydrate">
-                        <label class = "hydrate-container">Hydrate
+                        <label class = "hydrate-container">Is Hydrate?
                             <input type = "checkbox" id = "empirical-hydrate-check">
                             <span class = "checkmark"></span>
                         </label>
                     
                         <div class = "optional-hydrate-data">
                             <div class = "hydrate-mass">
-                                <span>Hydrate mass (Before burning)</span>
+                                <p>Hydrate mass (Before burning)</p>
                                 <input type = "text" id = "empirical-hydrate-mass" required>
                             </div>
                             <div class = "anhydrous-mass">
-                                <span>Anhydrous mass (After burning)</span>
-                                <input type = "text" id = "empirical-anhydrous-mass" requred>
+                                <p>Anhydrous mass (After burning)</p>
+                                <input type = "text" id = "empirical-anhydrous-mass" required>
                             </div>
                         </div>
                     </div>
+                </div>
+
+                <div class = "buttons">
+                    <input type = "button" class = "submit-button" id = "empirical-submit" value = "Get Empirical">
+                    <input type = "button" class = "clear-button" id = "empirical-clear" value = "Clear">
                 </div>
             </div>
         </div>
