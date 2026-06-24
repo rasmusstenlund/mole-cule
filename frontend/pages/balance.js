8c48a5fd5bc084ed0fb43648dddef968ad454e09
Rasmus Stenlund <rasmus.stenlund@gmail.com>
2026-06-24 22:30:51 +0200

    Add working add buttons for reactants and products with '+' signs in between

--- frontend/pages/balance.js
+++ frontend/pages/balance.js
@@ -5,15 +5,19 @@ export function page() {
                 <div class = "equation-maker">
                     <p>Reactants</p>
                     <div class = "equation-side" id = "balance-reactants-side">
-                        <input type = "text" class = "reactant-input">
-                        <input type = "button" id = "balance-add-reactant" class = "add-button" value = "+ Reactant">
+                        <div class = "equation-part-container">
+                            <input type = "text" class = "reactant-input">
+                        </div>
                     </div>
+                    <input type = "button" id = "balance-add-reactant" class = "add-button" value = "+ Reactant">
                     <p class = "arrow">&#8594</p>
                     <p>Products</p>
                     <div class = "equation-side" id = "balance-products-side">
-                        <input type = "text" class = "product-input">
-                        <input type = "button" id = "balance-add-product" class = "add-button" value = "+ Product">
+                        <div class = "equation-part-container">
+                            <input type = "text" class = "product-input">
+                        </div>
                     </div>
+                    <input type = "button" id = "balance-add-product" class = "add-button" value = "+ Product">
                 </div>
                 <div class = "buttons">
                     <input type = "button" class = "submit-button" id = "balance-submit" value = "Balance">
@@ -39,15 +43,42 @@ export function page() {
 export function setup() {
     const add_reactant = document.getElementById("balance-add-reactant");
     const add_product = document.getElementById("balance-add-product");
-    const reactant_list = document.getElementById("balance-reactants-side");
+    const reactants_list = document.getElementById("balance-reactants-side");
     const products_list = document.getElementById("balance-products-side");
 
 
     add_reactant.addEventListener("click", function () {
+        const last_input = reactants_list.lastElementChild;
+        const plus = document.createElement("p");
+        plus.textContent = "+";
+
+        last_input.appendChild(plus)
+
+        const div = document.createElement("div");
+        div.classList.add("equation-part-container");
+
         const input = document.createElement("input");
         input.type = "text";
         input.classList.add("reactant-input");
 
-        reactant_list.insertBefore(input, add_reactant);
+        div.appendChild(input)
+    
+        reactants_list.append(div)
+    })
+
+    add_product.addEventListener("click", function () {
+        const last_input = products_list.lastElementChild;
+        const plus = document.createElement("p");
+        plus.textContent = "+";
+        last_input.appendChild(plus)
+
+        const container = document.createElement("div");
+        container.classList.add("equation-part-container");
+        const input = document.createElement("input");
+        input.type = "text";
+        input.classList.add("product-input");
+        container.appendChild(input);
+
+        products_list.appendChild(container);
     })
 }
